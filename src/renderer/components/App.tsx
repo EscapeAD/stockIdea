bdc1424562235cc29279eba6546a6c52b64b4c36
Adam Tse <mr.adam.tse@gmail.com>
2020-04-18 18:58:33 +0800

    Create Electron React Webpack TS config

---
+++ src/renderer/components/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {hot} from "react-hot-loader/root";
+
+export default hot((): JSX.Element => 
+    (
+        <div>
+          <h1>
+            React Electon test
+          </h1>
+            <h1>💖 Loading</h1>
+        </div>
+    ));
